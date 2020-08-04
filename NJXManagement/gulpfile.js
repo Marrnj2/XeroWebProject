45713a514f84e9b5ce485929e8690c3a5cb3ca89
horrjd1 <jamie489horrell@gmail.com>
2020-08-04 14:42:30 +1200

    Renamed project, removed Docker, added material ui, added gulp

---
+++ NJXManagement/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require("gulp");
+
+
+function copy_node_modules(cb) {
+    return gulp
+        .src("node_modules/**")
+        .pipe(gulp.dest('wwwroot/node_modules'));
+
+    cb();
+}
+
+exports.copy_node_modules = copy_node_modules;
+exports.default = copy_node_modules;
---
+++ NJXManagement/wwwroot/js/home.jsx
@@ -0,0 +1,14 @@
+﻿import Button from '../../wwwroot/node_modules/@material-ui/core/Button';
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+                <div className="commentBox">
+                    testing
+                </div>
+        );
+    }
+}
+console.log('Testing hello yay')
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
