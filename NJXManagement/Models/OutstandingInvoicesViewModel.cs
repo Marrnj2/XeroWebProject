6c709ad1e0a4b23ea7309a37de35ba7dc7133c84
Nicholas Marr (1000015700) <MARRNJ2@student.op.ac.nz>
2020-08-13 15:59:35 +1200

    Added some basic auth still needs trouble shooting

---
+++ NJXManagement/Models/OutstandingInvoicesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NJXManagement.Models
+{
+    public class OutstandingInvoicesViewModel
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Data { get; set; }
+    }
+}
