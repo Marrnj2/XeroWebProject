4c59456fb71b5ed0e15b8a83dae25f3881162feb
horrjd1 <jamie489horrell@gmail.com>
2020-08-13 14:09:06 +1200

    created sql database project and made test tables

---
+++ NJXDatabase/TestEnrollment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TestEnrollment] (
+    [EnrollmentID] INT IDENTITY (1, 1) NOT NULL,
+    [Grade]        DECIMAL(3, 2) NULL,
+    [CourseID]     INT NOT NULL,
+    [StudentID]    INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([EnrollmentID] ASC),
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID]) 
+        REFERENCES [dbo].[TestCourse] ([CourseID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID]) 
+        REFERENCES [dbo].[TestStudent] ([StudentID]) ON DELETE CASCADE
+)
