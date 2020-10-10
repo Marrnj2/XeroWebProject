8b5246911bd14908a1dafe2a7fc6e45dad43af3b
Jamie Horrell <jamie489horrell@gmail.com>
2020-10-11 12:49:49 +1300

    Put components in responsive Material-ui grid

--- NJXManagement/ClientApp/src/components/Home.js
+++ NJXManagement/ClientApp/src/components/Home.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
 import BusinessBankAccount from "./Dashboard/BusinessBankAccount";
 import BusinessSavingsAccount from "./Dashboard/BusinessSavingsAccount";
 import TotalCashInAndOut from "./Dashboard/TotalCashInAndOut";
 import AccountWatchlist from "./Dashboard/AccountWatchlist";
 import Bills from "./Dashboard/Bills";
+import { withStyles } from '@material-ui/styles';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div className="cards">
-        <BusinessBankAccount/>
-        <BusinessSavingsAccount/>
-        <TotalCashInAndOut/>
-        <AccountWatchlist/>
-        <Bills/>
+      <div className="component-grid">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} md={6}>
+            <BusinessBankAccount/>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <BusinessSavingsAccount/>
+          </Grid>
+        </Grid>
       </div>
     );
   }
