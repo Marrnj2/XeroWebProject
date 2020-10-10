9ff0bc2e7119cb6df6b9716a4a3da2e103395027
Jamie Horrell <jamie489horrell@gmail.com>
2020-10-10 19:05:35 +1300

    Remove un-used components

--- NJXManagement/ClientApp/src/components/Layout.js
+++ NJXManagement/ClientApp/src/components/Layout.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
-import SideMenu from './side-menu';
 import './Layout.css';
 import ResponsiveDrawer from './SideMenu';
 
