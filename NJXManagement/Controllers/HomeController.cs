eb7498d2db01ca2583bcd6cae03c71d98e627e76
Nicholas Marr (1000015700) <MARRNJ2@student.op.ac.nz>
2020-10-12 13:36:00 +1300

    Getting Code from response url

--- NJXManagement/Controllers/HomeController.cs
+++ NJXManagement/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NJXManagement.Controllers
@@ -21,15 +23,20 @@ namespace NJXManagement.Controllers
         public IActionResult Path()
         {
             var url = "https://login.xero.com/identity/connect/authorize?response_type=code&client_id=F68F5B3DC51D422BA4A9CEBF499247CB&redirect_uri=https://localhost:5001/signin-oidc&scope=openid profile email accounting.transactions";
+
             return Redirect(url);
-
-
         }
         [Route("signin-oidc")]
         public IActionResult Next()
         {
             
-            return Content();
+            var text = HttpContext.Response;
+            var url = UriHelper.GetEncodedUrl(HttpContext.Request);
+            Uri u = new Uri(url);
+            string code = HttpUtility.ParseQueryString(u.Query).Get("code");
+
+
+            return Content("Hey");
         }
     }
 }
