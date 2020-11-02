b32ef5e169e4d57f53eecaa970b2a11b6de8843c
horrjd1 <jamie489horrell@gmail.com>
2020-11-02 14:17:13 +1300

    Swap DataGrid to basic table

--- NJXManagement/ClientApp/src/components/Employees/EmployeesTable.js
+++ NJXManagement/ClientApp/src/components/Employees/EmployeesTable.js
@@ -1,10 +1,15 @@
 import React, { useEffect,useState } from 'react';
-import { DataGrid } from '@material-ui/data-grid';
 import Paper from "@material-ui/core/Paper";
 import DeleteIcon from '@material-ui/icons/Delete';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AddEmployeeDialog from './AddEmployeeDialog'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -77,9 +82,30 @@ export default function EmployeesTable() {
     <Paper variant="outlined" className="card-paper">
     <h5>Employees</h5>
     <hr></hr>
-    <div style={{ height: 650, width: '100%' }}>
-      <DataGrid rows={employeeData} columns={columns} pageSize={15} checkboxSelection />
-    </div>
+    <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell align="left">Last Name</TableCell>
+            <TableCell align="left">Email</TableCell>
+            <TableCell align="left">Phone Number</TableCell>
+            <TableCell align="left">Start Date</TableCell>
+            <TableCell align="left">End Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employeeData.map((employee) => (
+            <TableRow key={employee.id}>
+              <TableCell component="th" scope="row">{employee.fname}</TableCell>
+              <TableCell align="left">{employee.lname}</TableCell>
+              <TableCell align="left">{employee.email}</TableCell>
+              <TableCell align="left">{employee.phone}</TableCell>
+              <TableCell align="left">{employee.startDate}</TableCell>
+              <TableCell align="left">{employee.endDate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     <Button
         variant="contained"
         color="secondary"
