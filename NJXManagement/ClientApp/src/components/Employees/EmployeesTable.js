c9d0ccb5ba0bbcaa4733429c0e69cb348e1bef9f
horrjd1 <jamie489horrell@gmail.com>
2020-11-02 14:18:12 +1300

    Removed delete button

--- NJXManagement/ClientApp/src/components/Employees/EmployeesTable.js
+++ NJXManagement/ClientApp/src/components/Employees/EmployeesTable.js
@@ -1,13 +1,10 @@
 import React, { useEffect,useState } from 'react';
 import Paper from "@material-ui/core/Paper";
-import DeleteIcon from '@material-ui/icons/Delete';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import AddEmployeeDialog from './AddEmployeeDialog'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
@@ -106,14 +103,6 @@ export default function EmployeesTable() {
           ))}
         </TableBody>
       </Table>
-    <Button
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        startIcon={<DeleteIcon />}
-      >
-        Delete
-      </Button>
       <AddEmployeeDialog/>
 
     </Paper>
