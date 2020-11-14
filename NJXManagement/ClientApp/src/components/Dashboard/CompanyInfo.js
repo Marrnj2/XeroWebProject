00d05315625da05f392a146fd79567e81dc8bcb9
Jamie Horrell <jamie489horrell@gmail.com>
2020-11-14 18:19:25 +1300

    Add info to company info component

--- NJXManagement/ClientApp/src/components/Dashboard/CompanyInfo.js
+++ NJXManagement/ClientApp/src/components/Dashboard/CompanyInfo.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import Moment from 'moment';
 
 export default function AccountWatchlist() {
   const [organisationData, setOrganisationData] = useState([]);
+  const [organisationAddress, setOrganisationAddress] = useState({});
 
   useEffect(() => {
     getOrganisationData();
@@ -17,16 +19,27 @@ export default function AccountWatchlist() {
     console.log(jsonData.Organisations[0]);
 
     setOrganisationData(jsonData.Organisations[0])
+
+    let address = {
+      AddressLine1: jsonData.Organisations[0].Addresses[0].AddressLine1,
+      AddressLine2: jsonData.Organisations[0].Addresses[0].AddressLine2,
+      PostalCode: jsonData.Organisations[0].Addresses[0].PostalCode,
+      City: jsonData.Organisations[0].Addresses[0].City,
+      CountryCode: jsonData.Organisations[0].CountryCode
+    }
+    setOrganisationAddress(address)
   };
 
   return (
     <Paper variant="outlined" className="card-paper">
-      <h5>Organisations</h5>
+      <h5>{organisationData.Name}</h5>
       <hr></hr>
-        <p>{organisationData.Name}</p>
-        <p>{organisationData.CountryCode}</p>
-        <p>{organisationData.FinancialYearEndDay}</p>
-        <p>{organisationData.FinancialYearEndMonth}</p>
+        <p>{organisationAddress.AddressLine1}<br></br>
+          {organisationAddress.AddressLine2}<br></br>
+          {organisationAddress.PostalCode + " " + organisationAddress.City}<br></br>
+          {organisationAddress.CountryCode}<br></br>
+        </p>
+        <p>End of finacial year:<br></br>{organisationData.FinancialYearEndDay + " " + Moment(organisationData.FinancialYearEndMonth).format("MMM") + " " + Moment().format('YYYY')}</p>
     </Paper>
   );
 }
